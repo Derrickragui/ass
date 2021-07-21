42aa0a888fdc340277023222cc352a206e28a99b
Derrickragui <derrickragui@gmail.com>
2021-07-21 11:33:54 +0300

    first commit

---
+++ class.py
@@ -0,0 +1,16 @@
+class animals:
+    typeProperty=""
+    characteristicsProperty=""
+    def __init__(self,type,characteristics):
+        self.typeProperty=type
+        self.characteristicsProperty=characteristics
+
+    def getcharacteristics(self):
+        characteristic1 = "reproducing"
+        characteristic2 = "feeding"
+        characteristic3 = "growing"
+        return self.characteristicsProperty
+        
+
+
+        
